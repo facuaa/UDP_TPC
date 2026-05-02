73510ae8390b341b89fd56e98bad9888b831805c
Facu <facundoa013@gmail.com>
2026-05-01 20:58:43 -0400

    Netcat python

--- TPC.py
+++ TPC.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host='www.google.com'
-target_port=80
+target_host='0.0.0.0'
+target_port=9998
 
 #creamos un objeto socket
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -10,7 +10,7 @@ client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect((target_host,target_port))
 
 #enviar algo de informacion
-client.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
+client.send(b"HOLA")
 
 #recibir algo de informacion
 response= client.recv(4096)
